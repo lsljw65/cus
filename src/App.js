9c0de01bfb50d847cba06d1aa06749eb74278cdd
lee <lsljw65@gmail.com>
2024-03-11 12:05:15 +0900

    ~4강

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+// import React, {Component} from 'react';
+import Customer from './components/Customer';
+
+const customer={
+  'name':'이종원',
+  'birthday':'961222',
+  'gender':"남성",
+  'job':"대학생"
+}
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
